3f8f56bb4408459601243fdd44561c3d94c3eef5
Никита Ростовский <rostovskiy.n.a@sberbank.ru>
2019-10-30 14:42:27 +0300

    Project init

---
+++ GapSlider/GapSlider/GapSlider.h
@@ -0,0 +1,18 @@
+//
+//  GapSlider.h
+//  GapSlider
+//
+//  Created by Nikita Rostovskii on 30/10/2019.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GapSlider.
+FOUNDATION_EXPORT double GapSliderVersionNumber;
+
+//! Project version string for GapSlider.
+FOUNDATION_EXPORT const unsigned char GapSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GapSlider/PublicHeader.h>
+
+
---
+++ GapSliderDemo/GapSliderDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GapSliderDemo
+//
+//  Created by Nikita Rostovskii on 30/10/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
